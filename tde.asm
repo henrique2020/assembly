06729335a4e61c24241986aa50da05e5bfb92541
JpEscobarM <jp.escobarmartins@gmail.com>
2025-10-24 17:05:52 -0300

    update Titulo,Botoes
    alterei a forma de escrita do titulo no segmento de dados, e desacoplei as funções de escrever titulo e botoes de dentro da proc JOGO

--- tde.asm
+++ tde.asm
@@ -1,25 +1,24 @@
 .model small
-.stack 100H
-.data
 
-arte_titulo db "   _____  _____  _____  _____           " ; , 10, 13 ; Isso quebra a linha
-            db "  /  ___>/     \/  _  \/  _  \          "            ; Verificar para usar na versao final
-            db "  |___  ||  |--||  _  <|  _  |          "
-            db "  <_____/\_____/\__|\_/\__|__/          "
-            db "          __  __  _____  ____   _____   "
-            db "         /  \/  \/  _  \/  _/  /   __\  "
-            db "         |  \/  ||  _  <|  |---|   __|  "
-            db "         \__ \__/\_____/\_____/\_____/  "
+
+.stack 100H
+
+
+.data
+      op_menu db 0
+      
+arte_titulo db 3 dup(" ")," ___                    _    _     ", 10, 13 ; , 10, 13 ; Isso quebra a linha
+            db 3 dup(" "),"/ __| __ _ _ __ _ _ __ | |__| |___ ", 10, 13            ; Verificar para usar na versao final
+            db 3 dup(" "),"\__ \/ _| '_/ _` | '  \| '_ \ / -_)", 10, 13
+            db 3 dup(" "),"|___/\__|_| \__,_|_|_|_|_.__/_\___|", 10, 13
+       
 tamanho_arte equ $ - arte_titulo
 
-arte_fase_1 db "       _____  _____  _____  _____       "
-            db "      /   __\/  _  \/  ___>/   __\      "
-            db "      |   __||  _  ||___  ||   __|      "
-            db "      \__/   \__|__/<_____/\_____/      "
-            db "                  ___                   "
-            db "                 /   |                  "
-            db "                  |  |                  "
-            db "                 <____>                 "
+arte_fase_1 db "   __                _ ", 10, 13
+            db "  / _|__ _ ___ ___  / |", 10, 13
+            db " |  _/ _` (_-</ -_) | |", 10, 13
+            db " |_| \__,_/__/\___| |_|", 10, 13
+
 
 arte_fase_2 db "       _____  _____  _____  _____       "
             db "      /   __\/  _  \/  ___>/   __\      "
@@ -50,48 +49,69 @@ arte_fase_3 db "       _____  _____  _____  _____       "
 
 
 
-btn_jogar db "                ",218,196,196,196,196,196,196,196,191,"               "
-          db "                ",179," JOGAR ",179,"               "               
-          db "                ",192,196,196,196,196,196,196,196,217,"               "
-tamanho_btn equ $ - btn_jogar
+btn_jogar db 15 dup(" "),218,196,196,196,196,196,196,196,191,10,13
+          db 15 dup(" "),179,           " JOGAR ",       179,10,13
+          db 15 dup(" "),192,196,196,196,196,196,196,196,217,10,13
+          
+tamanho_jogar equ $-btn_jogar ;$-> como se fosse um contador de posicao, ao montar uma string
+                              ;$ aponta para o final dela.
+                              
 
-btn_sair db "                ",218,196,196,196,196,196,196,196,191,"               "
-         db "                ",179," SAIR  ",179,"               "              
-         db "                ",192,196,196,196,196,196,196,196,217,"               "
+btn_sair  db 15 dup(" "),218,196,196,196,196,196,196,196,191,10,13
+          db 15 dup(" "),179,           " SAIR  ",        179,10,13
+          db 15 dup(" "),192,196,196,196,196,196,196,196,217,10,13
+
+
+tamanho_sair equ $-btn_sair
+
 
 menu_selecao db 0   ; 0 = Jogar, 1 = Sair
 inicia_jogo  db 0   ; Flag para iniciar o jogo
 fps dw 10000        ; tempo em microsegundos (/10 para frames por segundo)
 
 
-.code
+.code 
+
 ESCREVE_STRING proc ; Funcao que escreve strings na tela
-    push ES   
+    push AX
     push BX
+    push DS
+    push ES
+    push SI
+    push BP
 
-    mov BX, DS
-    mov ES, BX
-
-    mov AH, 13h
-    mov AL, 01h    
-    pop BX              
-    xor BH, BH
-
-    int 10h         ; Interrupcao para escrever a string
+    mov AH, 13h ;escreve string com atributos de cor
+    mov AL, 01h ;modo: atualiza o cursor apos a escrita   
+           
+    xor BH, BH  ;pagina de video 0
+    int 10h     ; Interrupcao para escrever a string
     
-    pop ES       
+    pop BP
+    pop SI
+    pop ES
+    pop DS
+    pop BX
+    pop AX
+           
     ret
 endp
 
 LIMPA_TELA proc
     push CX
-
-    mov CX, 64000
-    LIMPA_PIXEL:
-    mov SI, CX
-    mov WORD PTR ES:[SI], 0H
-    loop LIMPA_PIXEL
+    push AX
+    push DI
     
+    mov AX,0H
+    CLD ;zera o DF, DF = 0 avanca e DF = 1 volta
+    
+    
+    mov CX,4  ;como avanca +2 em stoswORD precisa percorrer 32k nao 64k
+    mov DI,0
+
+    rep stosw  ;repete CX vezes: [ES:DI] = AX; DI += 2. 
+    
+    pop DI
+    pop AX
     pop CX
     ret
 endp
@@ -108,10 +128,10 @@ endp
 
 JOGAR_SAIR proc                     ; Verifica qual opcao esta marcada
     cmp menu_selecao, 1
-    jne INICIA_JOGO
+    jne INICIA_JOGO_F1
     call TERMINA_JOGO
     
-    INICIA_JOGO:                    ; Limpa a tela e desenha a fase 1
+    INICIA_JOGO_F1:                    ; Limpa a tela e desenha a fase 1
         call LIMPA_TELA
         mov DH, 8
         mov DL, 0
@@ -145,13 +165,13 @@ VERIFICA_OPCAO proc         ; Verifica qual opcao esta marcada no menu (jogar/sa
     mov DL, 0
     mov BL, 0CH
     mov BP, offset btn_jogar
-    mov CX, tamanho_btn
+    mov CX, tamanho_jogar
     call ESCREVE_STRING
     
     mov DH, 21
     mov BL, 0FH
     mov BP, offset btn_sair
-    mov CX, tamanho_btn
+    mov CX, tamanho_sair
     call ESCREVE_STRING
     jmp VOLTAR_VERIFICA_OPCAO
     
@@ -160,13 +180,13 @@ VERIFICA_OPCAO proc         ; Verifica qual opcao esta marcada no menu (jogar/sa
         mov DL, 0
         mov BL, 0FH
         mov BP, offset btn_jogar
-        mov CX, tamanho_btn
+        mov CX, tamanho_jogar
         call ESCREVE_STRING
         
         mov DH, 21
         mov BL, 0CH
         mov BP, offset btn_sair
-        mov CX, tamanho_btn
+        mov CX, tamanho_sair
         call ESCREVE_STRING
     
     VOLTAR_VERIFICA_OPCAO:
@@ -225,27 +245,16 @@ endp
 JOGO proc                       ; Carrega a tela inicial do jogo (menu)
     call LIMPA_TELA
 
-    mov DH, 0
-    mov DL, 0
-    mov BL, 0AH
-    mov BP, offset arte_titulo
-    mov CX, tamanho_arte
-    call ESCREVE_STRING
     
     ;
     ; Fazer as artes das naves e meteoro
     ;
     
-    mov DH, 18
-    mov BL, 0CH
-    mov BP, offset btn_jogar
-    mov CX, tamanho_btn
-    call ESCREVE_STRING
     
     mov DH, 21
     mov BL, 15
     mov BP, offset btn_sair
-    mov CX, tamanho_btn
+    mov CX, tamanho_sair
     call ESCREVE_STRING
     
     MENU:
@@ -262,15 +271,85 @@ JOGO proc                       ; Carrega a tela inicial do jogo (menu)
     ret
 endp
 
+
+
+
+ 
+ESCREVE_TITULO proc ;prepara registradores pra executar o call print
+    
+    
+    mov AX,DS
+    mov ES,AX
+    
+    mov BP, offset arte_titulo
+    mov CX, tamanho_arte
+    mov BL, 02H ;representa a cor verde
+    xor DX,DX ;zerando o registrador DX -> DH=linha/DL=coluna
+    
+    call ESCREVE_STRING      
+        
+    
+    ret
+endp 
+    
+ESCREVE_BOTOES proc
+    push AX
+    
+    mov BL, 0FH;cor
+    mov AH, op_menu
+    
+    cmp AH, 0     
+    
+    jne INICIA_BTN
+    mov BL, 0CH
+
+INICIA_BTN:
+    
+    mov BP, offset btn_jogar 
+    mov CX, tamanho_jogar
+    
+    xor DL,DL ;coluna = 0
+    mov DH,18 ;linha = 18
+    
+    call ESCREVE_STRING
+    
+    mov BL, 0FH
+    mov AH, op_menu
+    cmp AH, 1
+    jne SAIR_BTN
+    mov BL, 0CH
+    
+SAIR_BTN:
+   
+   mov BP, offset btn_sair
+   mov CX, tamanho_sair
+   xor DL, DL ;colunha =0;
+   mov DH, 21 ;linha
+   
+   
+   pop AX      
+    
+   ret
+endp    
+    
 MAIN:
+    ;referencia o segmento de dados em ds
     mov AX, @data
     mov DS, AX
+    
+    
+    ;referencia o segmento de memoria de video em ES
     mov AX, 0A000H
     mov ES, AX
     
+    ;inicia modo de video com 0A000H
     xor AH, AH
     mov AL, 13H
     int 10H
+    
+    
+    call ESCREVE_TITULO
+    call ESCREVE_BOTOES
     
     call JOGO
    
